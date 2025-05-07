1dcbdf2a24afe7f812fc79142f98fd356c5c419d
Oscar Mier <voscarmv@gmail.com>
2025-05-06 20:32:37 -0500

    Layout skeleton

---
+++ src/reducers/index.js
@@ -0,0 +1,71 @@
+import { combineReducers } from 'redux';
+import { types } from '../constants/types';
+
+function login(state = {}, action) {
+    switch (action.type) {
+        case types.LOGIN_LOADING:
+            return {
+                loading: true,
+            };
+        case types.LOGIN_SUCCESS:
+            return {
+                loading: false,
+                data: action.payload
+            };
+        case types.LOGIN_ERROR:
+            return {
+                loading: false,
+                error: action.error
+            };
+        default:
+            return state;
+    }
+};
+
+function gallery(state = {}, action) {
+    switch (action.type) {
+        case types.GALLERY_LOADING:
+            return {
+                loading: true,
+            };
+        case types.GALLERY_SUCCESS:
+            return {
+                loading: false,
+                data: action.payload
+            };
+        case types.GALLERY_ERROR:
+            return {
+                loading: false,
+                error: action.error
+            };
+        default:
+            return state;
+    }
+};
+
+function picture(state = {}, action) {
+    switch (action.type) {
+        case types.PICTURE_LOADING:
+            return {
+                loading: true,
+            };
+        case types.PICTURE_SUCCESS:
+            return {
+                loading: false,
+                data: action.payload
+            };
+        case types.PICTURE_ERROR:
+            return {
+                loading: false,
+                error: action.error
+            };
+        default:
+            return state;
+    }
+};
+
+export default combineReducers({
+    loginStore: login,
+    galleryStore: gallery,
+    pictureStore: picture
+});
