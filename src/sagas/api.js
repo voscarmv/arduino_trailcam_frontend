916e297cdce5143bc857932c1f41c044123fb851
Oscar Mier <voscarmv@gmail.com>
2025-05-08 21:01:10 -0500

    API login function

--- src/sagas/api.js
+++ src/sagas/api.js
@@ -1,3 +1,30 @@
+dotenv.config();
+
+const apiurl = process.env.APIURL;
+const loginurl = `${apiurl}/login`;
+
+export const login = async (request, url = loginurl) => {
+    const response = await fetch(url, {
+        method: 'POST', // Use POST or another HTTP method as needed
+        headers: {
+          'Content-Type': 'application/json', // Set the content type
+        },
+        // body: JSON.stringify({ user: { email_address: user, password: password }}) // Send the body as JSON
+        body: JSON.stringify(request.data), // Send the body as JSON
+      });
+    return await response.json();
+}
+
+
+
+
+
+
+
+
+
+
+
 export const fetchCreate = async (request) => {
     const response = await fetch(
         request.url,
