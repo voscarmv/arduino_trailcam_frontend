543e678b3c7b0663787cf31b190dd31063132d43
Oscar Mier <voscarmv@gmail.com>
2025-05-09 23:58:07 -0500

    Deleted unnecessary actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,38 +4,3 @@ export const login = (record) => ({
     type: types.LOGIN,
     data: record.data,
 })
-
-// export const notification = (record) => ({
-//     type: types.NOTIFICATION,
-//     data: record.data,
-// })
-
-export const create = (record) => ({
-    type: types.CREATE,
-    table: record.table,
-    data: record.data,
-})
-
-export const read = (record) => ({
-    type: types.READ,
-    table: record.table,
-    id: record.id,
-})
-
-export const update = (record) => ({
-    type: types.UPDATE,
-    table: record.table,
-    data: record.data,
-    id: record.id,
-})
-
-export const deleter = (record) => ({
-    type: types.DELETE,
-    table: record.table,
-    id: record.id,
-})
-
-export const list = (record) => ({
-    type: types.LIST,
-    table: record.table,
-});
