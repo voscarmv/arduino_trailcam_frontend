02d2b415ea63ed3b6aceec714cff1d27182c1cd0
Oscar Mier <voscarmv@gmail.com>
2025-05-11 22:01:30 -0500

    NEWTOKEN

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,12 +2,14 @@ const LOGIN = "LOGIN";
 const GALLERY = "GALLERY";
 const PICTURE = "PICTURE";
 const REGENTOKEN = "REGENTOKEN";
+const NEWTOKEN = "NEWTOKEN";
 
 const types = {
     LOGIN,
     GALLERY,
     PICTURE,
     REGENTOKEN,
+    NEWTOKEN
 };
 
 export default types;
